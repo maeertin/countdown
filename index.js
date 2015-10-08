6ae31d60bf07875014a9fc0e585492f893fa8600
Martin Barri <maeertin@gmail.com>
2015-10-08 13:39:57 +0200

    Deleted unused file

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-	port    = process.env.PORT || 4444,
-	app     = express();
- 
-app.use('/', express.static(__dirname + '/'));
-app.use('/dist', express.static(__dirname + '/dist'));
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
-app.listen(port);
